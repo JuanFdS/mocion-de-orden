b21fcf2e10332a8c9d8b578c07dfafe7485b9c4b
juanFdS <juan9794@gmail.com>
2023-12-10 13:52:58 -0300

    Agregar porcentaje de carga

--- Titulo.gd
+++ Titulo.gd
@@ -2,6 +2,8 @@ extends Control
 
 const ASAMBLEA_PATH = "res://Asamblea.tscn"
 
+var load_progress = 0.0
+
 func _ready():
 	ResourceLoader.load_threaded_request(ASAMBLEA_PATH)
 	%Comenzar.pressed.connect(func():
@@ -9,13 +11,16 @@ func _ready():
 			ResourceLoader.load_threaded_get(ASAMBLEA_PATH)
 		)
 	)
+	%LoadUpdater.timeout.connect(self.update_load)
 
-func _process(_delta):
-	var load_status = ResourceLoader.load_threaded_get_status(ASAMBLEA_PATH)
+func update_load():
+	var progress = []
+	var load_status = ResourceLoader.load_threaded_get_status(ASAMBLEA_PATH, progress)
+	load_progress = max(progress[0], load_progress)
 	match load_status:
 		ResourceLoader.THREAD_LOAD_IN_PROGRESS:
 			%Comenzar.disabled = true
-			%Comenzar.text = "Cargando..."
+			%Comenzar.text = "Cargando... %.2f" % (load_progress * 100.0)
 		ResourceLoader.THREAD_LOAD_LOADED:
 			%Comenzar.disabled = false
 			%Comenzar.text = "¡Empezar!"
